5e9d1321a521571d1d3d7d75fcb906a4e04fa3a9
Alec Wong <aw685@cornell.edu>
2019-05-27 14:25:46 -0400

    07: added summary stats to document

--- 07-final_predictions.R
+++ 07-final_predictions.R
@@ -21,4 +21,64 @@ waic_df = data.frame(model = names(waic_vec), waic = waic_vec, row.names = NULL)
 
 waic_df
 
-# Select top models and multiply posterior dist together ---------------
+# Obtain parasite distribution models ----------------------------------
+
+
+# Summaries f magna -------------------------------------
+
+m_out = list.files('model_outputs/fmagna/', full.names = T)
+
+for(m in m_out){
+  load(m)
+}
+
+mod_list = list(
+  'null_model_ls'             = null_model_ls,
+  'full_model_ls'             = full_model_ls,
+  'full_model_elev_rw_ls'     = full_model_elev_rw_ls,
+  'red_mod_spatial_ls'        = red_mod_spatial_ls,
+  'red_mod_minus_speff_ls'    = red_mod_minus_speff_ls,
+  'red_mod_linear_all_cov_ls' = red_mod_linear_all_cov_ls,
+  'red_mod_survival_ls'       = red_mod_survival_ls
+)
+
+
+aic_vals_fmagna = sapply(mod_list, aicFunc)
+
+aic_vals_fmagna = data.frame("model" = names(mod_list), "waic" = aic_vals_fmagna) %>% arrange(waic)
+
+rm(list = mod_list %>% names) ; gc()
+
+aic_vals_fmagna
+
+# Summaries p tenuis models' waic -------------------------------------------------
+
+m_out = list.files('model_outputs/ptenuis/', full.names = T)
+
+for(m in m_out){
+  load(m)
+}
+
+mod_list = list(
+  'null_model_ls'             = null_model_ls,
+  'full_model_ls'             = full_model_ls,
+  'full_model_elev_rw_ls'     = full_model_elev_rw_ls,
+  'red_mod_spatial_ls'        = red_mod_spatial_ls,
+  'red_mod_minus_speff_ls'    = red_mod_minus_speff_ls,
+  'red_mod_linear_all_cov_ls' = red_mod_linear_all_cov_ls,
+  'red_mod_survival_ls'       = red_mod_survival_ls
+)
+
+
+aic_vals_ptenuis = sapply(mod_list, aicFunc)
+
+aic_vals_ptenuis = data.frame("model" = names(mod_list), "waic" = aic_vals_ptenuis) %>% arrange(waic)
+
+rm(list = mod_list %>% names) ; gc()
+
+aic_vals_ptenuis
+
+# Select top models and multiply posterior mean together ---------------
+
+# Note that for the parasite models prediction has already been done. In the stack, the 'pred' tag
+# should extract the mean predictions for the model.
