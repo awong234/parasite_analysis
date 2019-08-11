eed0e942102bfb51f7ec25b13e9874f813a2c189
Unknown <aw685@cornell.edu>
2019-08-10 22:38:49 -0400

    plotting
    both species

--- 07-postprocess.R
+++ 07-postprocess.R
@@ -52,10 +52,13 @@ save(aic_vals_fmagna, file = 'model_outputs/aicvals_fmagna.Rdata')
 
 # Plot fixed effects -----------------------------------
 
-fmagna_effects = effects_extr(mod_list, model_names = names(mod_list), var_order = rownames(full_model_ls$model$summary.fixed))
+varorder =  rownames(full_model_ls$model$summary.fixed)
+
+fmagna_effects = effects_extr(mod_list, model_names = names(mod_list), var_order = varorder)
 
 fmagna_effects$Fixed %<>% left_join(model_matches, by = c("name" = "fitnames"))
 
+Cairo::Cairo(width = 1100, height = 1600, file = 'images/parplot_fmagna.pdf', dpi = 110, type = 'pdf')
 ggplot(fmagna_effects$Fixed) + 
   geom_hline(yintercept = 0) + 
   geom_point(aes(x = variable, y = mode, color = pubnames, shape = pubnames), position = position_dodge(0.3)) + 
@@ -65,7 +68,12 @@ ggplot(fmagna_effects$Fixed) +
   guides(linetype = guide_legend(title = "Model"), shape = guide_legend(title = "Model"),  color = guide_legend(title = "Model")) +
   # coord_cartesian(ylim = c(-5, 5)) + 
   coord_flip(ylim = c(-5, 5)) +
-  theme_bw()
+  theme_bw() + theme(
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank()
+  )
+dev.off()
+
 
 # Get all the cpo and pit values for charts ------------
 
@@ -74,16 +82,19 @@ cpo_vals_fmagna = Map(f = function(mod, nam) {
   }, mod = mod_list, nam = names(mod_list)
 )
 
-cpo_vals_fmagna = do.call(what = rbind, args = cpo_vals_fmagna)
+cpo_vals_fmagna = do.call(what = rbind, args = cpo_vals_fmagna) %>% left_join(model_matches, by = c("model" = "fitnames"))
+
 rownames(cpo_vals_fmagna) = NULL
 
 cpo_vals_fmagna = reshape2::melt(cpo_vals_fmagna, id.vars = "pubnames", measure.vars = c("cpo", "pit"))
 
+Cairo::Cairo(width = 1024*2, height = 768*2, file = "images/cpo_fmagna.pdf", type = "pdf", dpi = 120)
 ggplot(cpo_vals_fmagna) + 
-  geom_density(aes(x = value, fill = model), alpha = 0.3) + 
+  geom_density(aes(x = value, fill = pubnames), alpha = 0.3) + 
   facet_wrap(~variable) + 
-  scale_fill_manual(values = colorspace::terrain_hcl(n = 7)) + 
+  scale_fill_viridis_d(direction = -1) + 
   theme_bw()
+dev.off()
 
 # Compare against fmagna values
 
@@ -110,6 +121,7 @@ mod_list = list(
   'full_model'             = full_model_ls,
   # 'full_model_elev_rw'     = full_model_elev_rw_ls,
   'red_mod_spatial'        = red_mod_spatial_ls,
+  'red_mod_spatial_elev'   = red_mod_spatial_elev_ls,
   # 'red_mod_minus_speff'    = red_mod_minus_speff_ls,
   'red_mod_linear_all_cov' = red_mod_linear_all_cov_ls,
   'red_mod_survival'       = red_mod_survival_ls
@@ -125,10 +137,11 @@ save(aic_vals_ptenuis, file = 'model_outputs/aicvals_ptenuis.Rdata')
 
 # Plot fixed effects -----------------------------------
 
-ptenuis_effects = effects_extr(mod_list, model_names = names(mod_list), var_order = rownames(full_model_ls$model$summary.fixed))
+ptenuis_effects = effects_extr(mod_list, model_names = names(mod_list), var_order = varorder)
 
 ptenuis_effects$Fixed %<>% left_join(model_matches, by = c("name" = "fitnames"))
 
+Cairo::Cairo(width = 1100, height = 1600, file = 'images/parplot_ptenuis.pdf', dpi = 110, type = 'pdf')
 ggplot(ptenuis_effects$Fixed) + 
   geom_hline(yintercept = 0) + 
   geom_point(aes(x = variable, y = mode, color = pubnames, shape = pubnames), position = position_dodge(0.3)) + 
@@ -138,7 +151,12 @@ ggplot(ptenuis_effects$Fixed) +
   guides(linetype = guide_legend(title = "Model"), shape = guide_legend(title = "Model"),  color = guide_legend(title = "Model")) +
   # coord_cartesian(ylim = c(-5, 5)) + 
   coord_flip(ylim = c(-5, 5)) +
-  theme_bw()
+  theme_bw() + theme(
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank()
+  )
+dev.off()
+
 
 # Get all the cpo and pit values for charts ------------
 
@@ -147,15 +165,25 @@ cpo_vals_ptenuis = Map(f = function(mod, nam) {
 }, mod = mod_list, nam = names(mod_list)
 )
 
-cpo_vals_ptenuis = do.call(what = rbind, args = cpo_vals_ptenuis)
+cpo_vals_ptenuis = do.call(what = rbind, args = cpo_vals_ptenuis) %>% left_join(model_matches, by = c("model" = "fitnames"))
 rownames(cpo_vals_ptenuis) = NULL
 
 cpo_vals_ptenuis = melt(cpo_vals_ptenuis, id.vars = "pubnames", measure.vars = c("cpo", "pit"))
 
-cpo_vals_ptenuis = reshape2::melt(cpo_vals_ptenuis, id.vars = "model")
-
+Cairo::Cairo(width = 1024*2, height = 768*2, file = "images/cpo_ptenuis.pdf", type = "pdf", dpi = 120)
 ggplot(cpo_vals_ptenuis) + 
-  geom_density(aes(x = value, fill = model), alpha = 0.3) + 
+  geom_density(aes(x = value, fill = pubnames), alpha = 0.3) + 
   facet_wrap(~variable) + 
   scale_fill_viridis_d(direction = -1) + 
+  theme_bw()
+dev.off()
+
+# Compare against ptenuis values
+
+combos = cpo_vals_ptenuis %>% select(model, variable) %>% unique %>% nrow
+
+cpo_vals_ptenuis %>% mutate(response = rep(data$dsl_mb, times = combos)) %>% 
+  ggplot() + 
+  geom_point(aes(x = response, y = value), shape = 1, alpha = 0.5) + 
+  facet_grid(model ~ variable) + 
   theme_bw()
